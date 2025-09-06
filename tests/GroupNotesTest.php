d71631f0cec06d6833b0569ddaebd1bdc9ef122e
giedrius <giedrius@inkodus.lt>
2025-09-06 22:47:20 +0300

    phpunit configuration

---
+++ tests/GroupNotesTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kukulis\PermissionBased\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class GroupNotesTest extends TestCase
+{
+
+    public function testGroupNotes() {
+        $this->assertTrue(true);
+    }
+}
